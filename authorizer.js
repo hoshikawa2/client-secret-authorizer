2a514f26494bff89632154ad621afa87525adaf4
Cristiano Hoshikawa <hoshikawa@uol.com.br>
2020-06-14 14:21:02 -0300

    adding nodeJS Scripts

---
+++ authorizer.js
@@ -0,0 +1,57 @@
+function authorizeClientSecret(token) {
+    //const expiryInterval = 3600000 // one hour expressed in miliseconds
+    const expiryInterval = 60000 // one minute expressed in miliseconds
+    const expiry = new Date(Date.now() + expiryInterval);
+    let json
+    // if the last character in the token is a number and the number is even, then authorization succeeds
+    const lastCharacter = token.slice(-1)
+    if (!isNaN(lastCharacter) && ((parseInt(lastCharacter) % 2) == 0)) {
+//        json = {
+//            "active": true,
+//            "principal": "hoshikawa@hotmail.com",
+//            "scope": ["list:showhtml", "read:showhtml", "create:showhtml", "update:showhtml", "delete:showhtml", "someScope"],
+//            "clientId": "clientIdFromHeader",
+//            "expiresAt": expiry.toISOString(),
+//            "context": {
+//                "key": "value", "email": "hoshikawa2@hotmail.com", "input": token
+//            }
+//        }
+        json =
+            {
+                "active": true,
+                "expiresAt": expiry.toISOString(),
+                "context": {
+                    "email": "james.doe@example.com"
+                },
+                "wwwAuthenticate": "Bearer realm=\"lucas.jellema.com\""
+            }
+    }
+    else
+        // simulate authorization error
+        json =
+            {
+                "active": false,
+                "expiresAt": expiry.toISOString(),
+                "context": {
+                    "email": "james.doe@example.com"
+                },
+                "wwwAuthenticate": "Bearer realm=\"lucas.jellema.com\""
+            }
+    return json
+}
+
+module.exports = {
+    authorizeClientSecret: authorizeClientSecret
+}
+
+// invoke on the command line with :
+// node authorizer '{"type":"TOKEN","token":"secret2"}'
+run = async function () {
+    if (process.argv && process.argv[2]) {
+        const input = JSON.parse(process.argv[2])
+        let response = authorizeClientSecret(input.type=='TOKEN'?input.token:input.header)
+        console.log("response: " + JSON.stringify(response))
+    }
+}
+
+run()
---
+++ func.js
@@ -0,0 +1,31 @@
+const fdk = require('@fnproject/fdk');
+const authorizer = require('./authorizer.js');
+
+fdk.handle(function (input, ctx) {
+  console.log(`Authorizer Function is invoked with input ${JSON.stringify(input)}`)
+  console.log(`Authorizer Function is invoked with ctx ${JSON.stringify(ctx)}`)
+  if (!input || !input.type || !(input.type == 'TOKEN' ? input.token : input.header)) {
+    // no token is passed in
+    const x =
+    {
+      "active": false,
+      "expiresAt": "2019-05-30T10:15:30+01:00",
+      "context": {
+        "email": "james.doe@example.com"
+        , "input": input
+      },
+      "wwwAuthenticate": "Bearer realm=\"provide.token.next.time.round\" "
+    }
+    return x
+  }
+  const x = authorizer.authorizeClientSecret(input.type == 'TOKEN' ? input.token : input.header)
+  ctx.setResponseHeader("Greeting", "Goedendag")
+  ctx.setResponseHeader("secret-header", input.header)
+  ctx.setResponseHeader("secret-token", input.token)
+  ctx.setResponseHeader("secret-type", input.type)
+  return x
+})
+
+// invoke with :
+// echo -n '{"type":"TOKEN","token":"secret2"}' | fn invoke lab-app client-secret-authorizer
+
